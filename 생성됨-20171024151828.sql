921685948be629a21bab3282669567a2d73aae7f
정석현 <31728357+ello603@users.noreply.github.com>
2017-10-24 15:39:07 +0900

    Add files via upload

---
+++ 생성됨-20171024151828.sql
@@ -0,0 +1,6 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 화요일-10월-24-2017   
+--------------------------------------------------------
+@C:\Users\neuroj\Desktop\ASYSCONTENTELEMENT.sql
+--@C:\Users\neuroj\Desktop\ASYSCONTENTELEMENT_DATA_TABLE.xml
+@C:\Users\neuroj\Desktop\ASYSCONTENTELEMENT_CONSTRAINT.sql
